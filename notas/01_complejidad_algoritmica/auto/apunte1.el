3d84ef792bc3b8398b2a36d74ad5106abe91e740
Victor Torres <victor.torrest@ciencias.unam.mx>
2025-12-06 00:06:46 -0600

    Agregando las notas de complejidad computacional

---
+++ notas/01_complejidad_algoritmica/auto/apunte1.el
@@ -0,0 +1,15 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "apunte1"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"))
+ :latex)
+
